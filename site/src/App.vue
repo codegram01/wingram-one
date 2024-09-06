1bac87b435317abea12bbc838887dbbfb24edf39
codegram01 <wuyen.mail@gmail.com>
2024-09-06 07:27:33 +0700

    init: frontend

---
+++ site/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import NavbarLayout from '@/components/layouts/NavbarLayout.vue';
+
+</script>
+
+<template>
+  <div class="app">
+    <div class="navbar">
+      <NavbarLayout/>
+    </div>
+    <div class="main">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ site/src/components/layouts/NavbarLayout.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
